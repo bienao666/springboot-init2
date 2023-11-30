cb71b4610bb31a98e6b2651f89a0b627c743d7a7
tiandawei <tdw970122@gmail.com>
2023-11-30 14:47:07 +0800

    获取用户的路由

--- src/main/java/life/bienao/springbootinit/config/WebConfig.java
+++ src/main/java/life/bienao/springbootinit/config/WebConfig.java
@@ -38,6 +38,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("forward:/index.html");
+        registry.addViewController("/view/**").setViewName("forward:/index.html");
     }
 
 
